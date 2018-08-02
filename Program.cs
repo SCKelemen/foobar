af4c3984b7e18f561641d86d19adee745919ab08
Samuel Kelemen <Samuel@Kelemen.us>
2018-08-02 13:44:09 -0400

    init

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace foobar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Foobar foo = new Foobar();
+            Processor processor = new Processor();
+            processor.Process(foo);
+            Console.WriteLine($"Foobar.Bar value = {foo.Bar}");
+            Console.ReadKey();
+        }
+    }
+
+    class Foobar {
+        public string Bar = "FooBar";
+    }
+
+    class Processor {
+        public void Process(Foobar foo) {
+            foo.Bar = "some text";
+        }
+    }
+}
